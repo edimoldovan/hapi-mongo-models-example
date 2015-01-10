5f0f249e2b09a66002576123bd2dc24a2707924d
Moldován Eduárd <edimoldovan@gmail.com>
2015-01-10 21:55:50 +0100

    Fix model

--- models/users.js
+++ models/users.js
@@ -1,17 +1,21 @@
 var Joi = require('joi');
 var BaseModel = require('hapi-mongo-models').BaseModel;
+var ObjectAssign = require('object-assign');
 
 var User = BaseModel.extend({
-    // instance prototype
+    constructor: function (attrs) {
+
+        ObjectAssign(this, attrs);
+    }
 });
 
 User._collection = 'users'; // the mongo collection name
 
 User.schema = Joi.object().keys({
-	name: Joi.string(),
-	image: Joi.string(),
-	password: Joi.string().required(),
-	email: Joi.string().email().required()
+    name: Joi.string(),
+    image: Joi.string(),
+    password: Joi.string().required(),
+    email: Joi.string().email().required()
 });
 
 module.exports = User;
